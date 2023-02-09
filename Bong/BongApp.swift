c4264e3fd9bf1291766abb5c72687e9a483d8feb
Mathias Raa <skraagen@hotmail.com>
2023-02-08 23:47:51 -0800

    Initial Commit

---
+++ Bong/BongApp.swift
@@ -0,0 +1,17 @@
+//
+//  BongApp.swift
+//  Bong
+//
+//  Created by Mathias Raa on 08/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct BongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
