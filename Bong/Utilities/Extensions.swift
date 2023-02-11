4e43f5b9821925812d6c2d47731c2979b0ece5b9
Nick <nickask12@gmail.com>
2023-02-10 23:25:11 -0800

    Done with cameraView

---
+++ Bong/Utilities/Extensions.swift
@@ -0,0 +1,21 @@
+//
+//  Extensions.swift
+//  Bong
+//
+//  Created by Nick Askari on 10/02/2023.
+//
+
+import SwiftUI
+import Foundation
+
+extension Text {
+    func capsuleStyle(_ color: Color) -> some View {
+        self
+            .padding(EdgeInsets(top: 20, leading: 60, bottom: 20, trailing: 60))
+            .foregroundColor(.white)
+            .background(Capsule().foregroundColor(color))
+            .shadow(radius: 5)
+    }
+}
+
+
